69cfb34f261afed2fe67df1bcbe1a7cbb111694c
Yixuan Zhou <haizhuolaojisite@gmail.com>
2024-07-16 21:28:29 -0400

    update train eval for LR

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,14 @@
 import numpy as np
-
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
+# from LinearRegression import LinearRegression
+n_samples = 1000
+n_features = 10
+X, y = make_regression(n_samples=n_samples, n_features=n_features,
+                       n_informative=5, noise=10, random_state=42)
+X_fit, X_val, y_fit, y_val = train_test_split(X, y, test_size=.2, random_state=1)
 
 class LinearRegression:
     def __init__(self, lr=0.001, n_iters=1000):
@@ -31,3 +40,25 @@ class LinearRegression:
 
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
+
+
+model = LinearRegression(lr=0.01)
+model.fit(X_fit, y_fit)
+train_predictions = model.predict(X_fit)
+
+
+def mse(y, predictions):
+    return np.mean(np.square(y - predictions))
+
+
+print(mse(y_fit, train_predictions))
+
+y_pred_train = model.predict(X_fit)
+y_pred_val = model.predict(X_val)
+
+fig = plt.figure(figsize=(6,4))
+plt.scatter(y_val, y_pred_val, color="green", s=5)
+plt.plot([min(y), max(y)], [min(y), max(y)], color="red", linewidth=2)
+plt.xlabel("True values")
+plt.ylabel("Predicted validation values")
+plt.show()
